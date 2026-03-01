ad9241fbced861d19ee75b5401afecb0e149b412
KhaledCS1 <10688@holbertonstudents.com>
2026-03-01 08:45:17 +0300

    Update

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-whois "$1" | awk -F': ' '/^Registrant |^Admin |^Tech / {gsub(/^[ \t]+|[ \t]+$/, "", $2); if ($1 ~ /Street/) $2=$2" "; print $1 "," $2}' > "$1.csv"
+whois "$1" | awk -F': ' '/^Registrant |^Admin |^Tech / {gsub(/^[ \t]+|[ \t]+$/, "", $2);
+if ($1 ~ /Street/) $2=$2" "; if ($1 ~ /Ext$/) {print $1 ":," $2} else {print $1 "," $2}}' \
+| awk 'NR>1{print prev} {prev=$0} END{printf "%s", prev}' > "$1.csv"
